7b7f4558c1c8180fb4634a032d86e76f39913ff8
Маки Макиев <useful22052@gmail.com>
2023-05-03 20:30:00 +0300

    routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const app = express();
-//
-//
-// app.use(express.static('pages'));
-//
-// app.get('/', (req, res) => {
-//
-//     res.sendFile(__dirname + '/index.html')
-//
-// });
-//
-// app.listen(3000, () => {
-//     console.log('Server started on http://localhost:3000');
-// });
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('pages'));
+
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/index.html')
+
+});
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
