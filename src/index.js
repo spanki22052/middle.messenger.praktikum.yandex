e8137e4d8b6eda3445b8ace853ed2157f88da97c
Маки Макиев <useful22052@gmail.com>
2023-05-03 20:48:17 +0300

    sd

--- src/index.js
+++ src/index.js
@@ -7,3 +7,7 @@ app.use(express.static(`./dist/`));
 app.listen(3000, () => {
     console.log(`App listening on port 3000!`);
 });
+
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
